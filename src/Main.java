d5ee13cfdd274467225e1fdb27704a5097077554
Aleksey Zinov <AZinov@inno.tech>
2024-08-06 15:59:46 +0300

    add class line

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,44 @@
 public class Main {
     public static void main(String[] args) {
         Point point1 = new Point(1,3);
-        Point point2 = new Point(1,3);
-        Point point3 = new Point(5,8);
-        System.out.println(point1);
-        System.out.println(point2);
-        System.out.println(point3);
-        System.out.println(equalsPoints(point1, point2));
-        System.out.println(equalsPoints(point2, point3));
-        System.out.println(equalsPoints(point1, point3));
+        Point point2 = new Point(5,3);
+        Point point3 = new Point(10,11);
+        Point point4 = new Point(15, 19);
+        Line line1 = new Line(point1, point2);
+        Line line2 = new Line(point3, point4);
+        Line line3 = new Line(point2, point3);
 
+        System.out.println("Исходные линии");
+        System.out.println(line1);
+        System.out.println(line2);
+        System.out.println(line3);
+        System.out.println ("Общая длина всех линий: " +
+                getLinesLength(line1.getLength(point1,point2),
+                        line2.getLength(point3,point4),
+                        line3.getLength(point2,point3)));
+        System.out.println("");
+
+        System.out.println("Линии после изменения");
+        changingCoordinatesPoint(line3);
+        System.out.println(line1);
+        System.out.println(line2);
+        System.out.println(line3);
+
+        System.out.println ("Общая длина всех линий: " +
+                getLinesLength(line1.getLength(point1,point2),
+                        line2.getLength(point3,point4),
+                        line3.getLength(point2,point3)));
     }
-    public static boolean equalsPoints (Point pointOne, Point pointTwo) {
-        if (pointOne == pointTwo) {
-            return true;
-        } else return false;
+
+    public static double getLinesLength (double length1, double length2 , double length3) {
+        double res = (Math.ceil((length1 + length2 + length3) * 100)) / 100;
+        return res;
+    }
+
+    public static void changingCoordinatesPoint (Line line3) {
+        line3.point1.x += 2;
+        line3.point1.y += 3;
+        line3.point2.x -= 1;
+        line3.point2.y -= 4;
     }
 }
